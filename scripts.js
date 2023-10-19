1f398e02bb58a38e1f11448cce37c61b43e12e87
Lucas <41449194+lucasph123@users.noreply.github.com>
2023-10-19 19:31:13 -0300

    added functions to the buttons added basic operations add,sub,div,multi,delete,equal added results and numbers in the display

--- scripts.js
+++ scripts.js
@@ -1,9 +1,103 @@
+let firstNum = '';
+let operator = null;
+let secondNum = '';
 
-const btn = document.querySelector('button');
+
 const display = document.querySelector('.display');
+const btn = document.querySelectorAll('button');
+const delBtn = document.querySelector('.delete');
+const backBtn = document.querySelector('.back')
 
-//só ta pegando o 1 pq nao colocquei query selector all, mas nao funcionas
-    btn.addEventListener('click', function(e)
-    {''
-        display.textContent = e.textContent;
-    } );
+
+function operate()
+{
+    for(i=0;i<btn.length;i++)
+    {   
+        btn[i].addEventListener('click',(e)=>{
+            if(e.target.className == 'number')
+            {
+                
+                if(operator == null){
+                    firstNum += e.target.value;
+                    display.textContent = firstNum;
+                }
+                else
+                {
+                    secondNum += e.target.value;
+                    display.textContent = secondNum;
+                }
+            }
+            else
+            {
+                if(secondNum != '' && firstNum != '')
+                {
+                    if(e.target.value == '=')
+                    {
+                        if(operator == '*')  multiply(firstNum,secondNum);
+                        if(operator == '+')  sum(firstNum,secondNum);
+                        if(operator == '/')  division(firstNum,secondNum);
+                        if(operator == '-')  subtract(firstNum,secondNum);
+                        
+                    }
+                    else
+                    { 
+                        if(operator == '*')  multiply(firstNum,secondNum);
+                        if(operator == '+')  sum(firstNum,secondNum);
+                        if(operator == '/')  division(firstNum,secondNum);
+                        if(operator == '-')  subtract(firstNum,secondNum);
+                        
+                    }
+                }
+                    if(firstNum != '') operator = e.target.value;
+                
+                    
+            }
+               
+        });
+    }
+}
+
+operate();
+
+function multiply(num1,num2){
+    firstNum = parseFloat(num1)*parseFloat(num2);
+    secondNum = '';
+    operator = '';
+    return display.textContent = firstNum;
+};
+  
+function sum(num1,num2){
+    firstNum = parseFloat(num1)+parseFloat(num2);
+    secondNum = '';
+    operator = '';
+    return display.textContent = firstNum;
+};
+function subtract(num1,num2){
+    firstNum = parseFloat(num1)-parseFloat(num2);
+    secondNum = '';
+    operator = '';
+    return display.textContent = firstNum;
+};
+function division(num1,num2){
+    firstNum = parseFloat(num1)/parseFloat(num2);
+    secondNum = '';
+    operator = '';
+    return display.textContent = firstNum;
+};
+
+
+delBtn.addEventListener('click', () => {
+    firstNum = '';
+    operator = null;
+    secondNum = '';
+    display.textContent = '';
+})
+
+backBtn.addEventListener('click', () => {
+
+    console.log(firstNum);
+    firstNum = firstNum.substring(0, firstNum.length - 1);
+    display.textContent = firstNum;
+
+})
+ 
